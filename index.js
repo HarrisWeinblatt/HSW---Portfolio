a82c427a31d02e6d1c0992bff6e398bb66631173
HarrisWeinblatt <110180480+HarrisWeinblatt@users.noreply.github.com>
2022-10-17 12:04:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,3 @@ var myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.bs.modal', function () {
   myInput.focus()
 });
-
-$('.dropdown a').click(function(e) {
-  e.preventDefault();
-  setTimeout($.proxy(function() {
-    if ('ontouchstart' in document.documentElement) {
-      $(this).siblings('.dropdown-backdrop').off().remove();
-    }
-  }, this), 0);
-});

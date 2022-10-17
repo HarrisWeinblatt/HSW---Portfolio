501a836beca1e10a5511433f3fabe1a5b91fdf0f
HarrisWeinblatt <110180480+HarrisWeinblatt@users.noreply.github.com>
2022-10-17 11:43:44 -0700

    navbar mobile

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ var myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.bs.modal', function () {
   myInput.focus()
 });
+
+$('.dropdown a').click(function(e) {
+  e.preventDefault();
+  setTimeout($.proxy(function() {
+    if ('ontouchstart' in document.documentElement) {
+      $(this).siblings('.dropdown-backdrop').off().remove();
+    }
+  }, this), 0);
+});

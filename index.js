06ead77915535ed962ff1852c490e256f8c78c9d
HarrisWeinblatt <110180480+HarrisWeinblatt@users.noreply.github.com>
2022-10-20 19:25:50 -0700

    javascript with ball on banner

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+// ball function to play with on banner
+
+let ball = document.getElementById('float-circle');
+
+function up() {
+  ball.style.bottom = '500px';
+}
+function down() {
+  ball.style.bottom = '50px';
+}
+document.addEventListener('mousedown', up);
+document.addEventListener('mouseup', down);
+
+// opens up my resume in a modal box
+
 var myModal = document.getElementById('myModal')
 var myInput = document.getElementById('myInput')
 
 myModal.addEventListener('shown.bs.modal', function () {
   myInput.focus()
 });
+
+

334163f2c89c6edb0158c6755bb8278eb93ceb13
HarrisWeinblatt <110180480+HarrisWeinblatt@users.noreply.github.com>
2022-10-24 12:06:44 -0700

    no bouncing ball

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-// ball function to play with on banner
-
-let ball = document.getElementById('float-circle');
-
-function up() {
-  ball.style.bottom = '350px';
-}
-function down() {
-  ball.style.bottom = '50px';
-}
-document.addEventListener('mousedown', up);
-document.addEventListener('mouseup', down);
-
 // opens up my resume in a modal box
 
 var myModal = document.getElementById('myModal')

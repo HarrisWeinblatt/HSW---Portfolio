55018b44c331d3ccfe32fdef20e392e423210322
HarrisWeinblatt <110180480+HarrisWeinblatt@users.noreply.github.com>
2022-10-31 23:25:35 -0700

    back to top button

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+/*Scroll to top when arrow up clicked BEGIN*/
+$(window).scroll(function() {
+  var height = $(window).scrollTop();
+  if (height > 100) {
+      $('#back2Top').fadeIn();
+  } else {
+      $('#back2Top').fadeOut();
+  }
+});
+$(document).ready(function() {
+  $("#back2Top").click(function(event) {
+      event.preventDefault();
+      $("html, body").animate({ scrollTop: 0 }, "slow");
+      return false;
+  });
+
+});
+/*Scroll to top when arrow up clicked END*/
+
 // opens up my resume in a modal box
 
 var myModal = document.getElementById('myModal')

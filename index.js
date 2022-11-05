c2e7d0ecd0c624ed2bf17eac3a5ba44f52c14b9c
HarrisWeinblatt <110180480+HarrisWeinblatt@users.noreply.github.com>
2022-11-05 00:56:30 -0700

    tip calculator complete

--- index.js
+++ index.js
@@ -274,9 +274,34 @@ window.onload = function() {
   document.getElementById("generate").addEventListener('click', generateQuote);
 }
 
-// date counter javascript is in html
+// javascript for date counter is in html
 
 
+// Tip Calulator
+let billAmount = document.getElementById('bill-amount');
+let percentTip = document.getElementById('percentage-tip');
+let tipAmount = document.getElementById('tip-amount');
+let total = document.getElementById('total');
+let calculate = document.getElementById('calculate');
+
+calculate.addEventListener('click', function(){
+    billAmount = Number(billAmount.value);
+    percentTip = Number(percentTip.value);
+
+    if (billAmount <= 0 || billAmount === null){
+        alert('Please enter a valid Bill Amount')
+    }
+    else if (percentTip <= 0 || percentTip === null){
+        alert('Please enter a valid Tip Percentage')
+    }
+    else {
+        let calculatedTip =  percentTip/100*billAmount;
+        tipAmount.value = '$' + calculatedTip;
+
+        let calculatedTotal = billAmount + calculatedTip;
+        total.value = '$' + calculatedTotal;
+    }
+})
 
 // opens up my resume in a modal box
 
